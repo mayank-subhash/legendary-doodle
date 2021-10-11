a7799b996d1d80e115d8b72df89c267f2da2dc36
mayank-subhash <77912689+mayank-subhash@users.noreply.github.com>
2021-10-11 18:27:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    Image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera")
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '"/>';
+
+        function take_snapshot() {
+            Webcam.snap(function (data_uri) {
+                document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '"/>';
+            });
+        }
+    });
+}
+console.log('ml5 version:', ml5.version);
+ // Initialize the Image Classifier method with MobileNet //
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+   // When the model is loaded 
+   function modelLoaded() { console.log('Model Loaded!'); }
